aadb3bbe07092d90e58ae8d33887eb44d4904841
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-04-23 21:08:59 +0300

    added requests to api, films list

--- src/components/homeScreen/HomeScreen.js
+++ src/components/homeScreen/HomeScreen.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Banner } from "../banner/Banner";
 import { Navbar } from "../navbar/Navbar";
+import requests from "../../api/Requests";
+import { Row } from "./Row";
 
 export const HomeScreen = () => {
   return (
     <div>
       <Navbar />
-      {/* banner */}
+      <Banner />
 
-      {/* rows */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Tranding Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
