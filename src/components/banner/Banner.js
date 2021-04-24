da6b390a87f32db21ea0a55c186f54c5f809aa72
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-04-24 10:23:31 +0300

    deployed on firebase, fixed README.md

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -40,7 +40,7 @@ export const Banner = () => {
           <button className="banner__button">Play</button>
           <button className="banner__button">My list</button>
         </div>
-        <h1 className="banner__description">{truncate(movie.overview, 120)}</h1>
+        <h1 className="banner__description">{truncate(movie.overview, 150)}</h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
